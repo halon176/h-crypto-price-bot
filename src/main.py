867f1151c45f1960ef4b914229b9ed6d23db830e
halon176 <acc.github@halon.cc>
2023-04-08 16:01:45 +0200

    added markdown formatting link on homepage and twitter

--- src/main.py
+++ src/main.py
@@ -66,6 +66,9 @@ async def get_crypto_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
     market_cap_rank = crypto_data['market_cap_rank']
     crypto_name = crypto_data['name']
     crypto_price = humanize.intcomma(crypto_data['market_data']['current_price']["usd"])
+    web = crypto_data['links']['homepage'][0]
+    twitter = "https://twitter.com/" + crypto_data['links']["twitter_screen_name"]
+    symbol = (crypto_data['symbol']).upper()
 
     price_change_percentage_24h = round(crypto_data['market_data']['price_change_percentage_24h'], 1)
     price_change_percentage_7d = round(crypto_data['market_data']['price_change_percentage_7d'], 1)
@@ -78,16 +81,17 @@ async def get_crypto_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
     market_cap = humanize.intword(crypto_data['market_data']['market_cap']["usd"])
     circulating_supply = humanize.intword(crypto_data['market_data']['circulating_supply'])
     total_supply = humanize.intword(crypto_data['market_data']['total_supply'])
-    ath = humanize.intword(crypto_data['market_data']['ath']["usd"])
+    ath = humanize.intcomma(crypto_data['market_data']['ath']["usd"])
     ath_change_percentage = round(crypto_data['market_data']['ath_change_percentage']["usd"], 1)
     ath_date = format_date(crypto_data['market_data']['ath_date']["usd"])
-    atl = humanize.intword(crypto_data['market_data']['atl']["usd"])
+    atl = humanize.intcomma(crypto_data['market_data']['atl']["usd"])
     atl_change_percentage = round(crypto_data['market_data']['atl_change_percentage']["usd"], 1)
     atl_date = format_date(crypto_data['market_data']['atl_date']["usd"])
 
-    message = f"{market_cap_rank}° {crypto_name} ({crypto_symbol})\n" \
-              f"Price: 💰{crypto_price}$\n" \
-              f"----------------------------\n" \
+    message = f"{market_cap_rank}° [{crypto_name}]({web}) [{symbol}]({twitter})\n" \
+              f"```\n" \
+              f"Price: {crypto_price}$\n" \
+              f"-----------------------------\n" \
               f"24h:     {price_change_percentage_24h}%\n" \
               f"7d:      {price_change_percentage_7d}%\n" \
               f"14d:     {price_change_percentage_14d}%\n" \
@@ -95,14 +99,18 @@ async def get_crypto_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
               f"60d:     {price_change_percentage_60d}%\n" \
               f"200d:    {price_change_percentage_200d}%\n" \
               f"1y:      {price_change_percentage_1y}%\n" \
-              f"----------------------------\n" \
-              f"💰 Market Cap  {market_cap}\n" \
+              f"-----------------------------\n" \
+              f"💰 M. Cap     {market_cap}\n" \
               f"💵 Circ. S     {circulating_supply}\n" \
               f"🖨 Total S    {total_supply}\n" \
-              f"📈 ATH  {ath}$  ({ath_change_percentage}%) {ath_date}  \n" \
-              f"📉 ATL  {atl}$  ({atl_change_percentage}%) {atl_date}   \n"
+              f"📈 ATH  {ath}$  ({ath_change_percentage}%) {ath_date}\n" \
+              f"📉 ATL  {atl}$  ({atl_change_percentage}%) {atl_date}\n" \
+              f"```"
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=message,
+                                   parse_mode="markdown",
+                                   disable_web_page_preview=True)
 
 
 if __name__ == '__main__':

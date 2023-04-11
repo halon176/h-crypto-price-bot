3188aa478f5cdf6371b44bc9b2c0275c822900e0
halon176 <acc.github@halon.cc>
2023-04-11 17:12:20 +0200

    add error handling for null values response JSON

--- src/cg_calls.py
+++ src/cg_calls.py
@@ -58,8 +58,10 @@ async def get_cg_price(coin, update: Update, context: ContextTypes.DEFAULT_TYPE)
     web = crypto_data['links']['homepage'][0]
     twitter = "https://twitter.com/" + crypto_data['links']["twitter_screen_name"]
     symbol = (crypto_data['symbol']).upper()
-
-    price_change_percentage_24h = round(crypto_data['market_data']['price_change_percentage_24h'], 1)
+    if not crypto_data['market_data']['price_change_percentage_24h']:
+        price_change_percentage_24h = "N/A"
+    else:
+        price_change_percentage_24h = round(crypto_data['market_data']['price_change_percentage_24h'], 1)
     price_change_percentage_7d = round(crypto_data['market_data']['price_change_percentage_7d'], 1)
     price_change_percentage_14d = round(crypto_data['market_data']['price_change_percentage_14d'], 1)
     price_change_percentage_30d = round(crypto_data['market_data']['price_change_percentage_30d'], 1)
@@ -67,15 +69,30 @@ async def get_cg_price(coin, update: Update, context: ContextTypes.DEFAULT_TYPE)
     price_change_percentage_200d = round(crypto_data['market_data']['price_change_percentage_200d'], 1)
     price_change_percentage_1y = round(crypto_data['market_data']['price_change_percentage_1y'], 1)
 
-    market_cap = humanize.intword(crypto_data['market_data']['market_cap']["usd"])
+    if not crypto_data['market_data']['market_cap']:
+        market_cap = "N/A"
+    else:
+        market_cap = humanize.intword(crypto_data['market_data']['market_cap']["usd"])
     circulating_supply = humanize.intword(crypto_data['market_data']['circulating_supply'])
     total_supply = humanize.intword(crypto_data['market_data']['total_supply'])
-    ath = humanize.intcomma(crypto_data['market_data']['ath']["usd"])
+    if not crypto_data['market_data']['ath']["sgd"]:
+        ath = "N/A"
+    else:
+        ath = humanize.intcomma(crypto_data['market_data']['ath']["usd"])
     ath_change_percentage = round(crypto_data['market_data']['ath_change_percentage']["usd"], 1)
-    ath_date = format_date(crypto_data['market_data']['ath_date']["usd"])
-    atl = humanize.intcomma(crypto_data['market_data']['atl']["usd"])
+    if not crypto_data['market_data']['ath_date']['sgd']:
+        ath_date = "N/A"
+    else:
+        ath_date = format_date(crypto_data['market_data']['ath_date']["usd"])
+    if not crypto_data['market_data']['atl']:
+        atl = "N/A"
+    else:
+        atl = humanize.intcomma(crypto_data['market_data']['atl']["usd"])
     atl_change_percentage = round(crypto_data['market_data']['atl_change_percentage']["usd"], 1)
-    atl_date = format_date(crypto_data['market_data']['atl_date']["usd"])
+    if not crypto_data['market_data']['atl_date']:
+        atl_date = "N/A"
+    else:
+        atl_date = format_date(crypto_data['market_data']['atl_date']["usd"])
 
     message = f"{market_cap_rank}° [{crypto_name}]({web}) [{symbol}]({twitter})\n" \
               f"```\n" \
@@ -92,8 +109,8 @@ async def get_cg_price(coin, update: Update, context: ContextTypes.DEFAULT_TYPE)
               f"💰 M. Cap     {market_cap}\n" \
               f"💵 Circ. S    {circulating_supply}\n" \
               f"🖨 Total S    {total_supply}\n" \
-              f"📈 ATH  {ath}$  ({ath_change_percentage}%) {ath_date}\n" \
-              f"📉 ATL  {atl}$  ({atl_change_percentage}%) {atl_date}\n" \
+              f"📈 ATH {ath}$ ({ath_change_percentage}%) {ath_date}\n" \
+              f"📉 ATL {atl}$ ({atl_change_percentage}%) {atl_date}\n" \
               f"```"
 
     await context.bot.send_message(chat_id=update.effective_chat.id,

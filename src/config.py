fabb7f117b97b22abb07b9a9f6174eb2be1e469e
halon176 <acc.github@halon.cc>
2023-04-22 11:24:54 +0200

    removed dotenv to manage token with environment variables

--- src/config.py
+++ src/config.py
@@ -1,9 +1,3 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 TOKEN = os.environ.get("TOKEN")
-BINANCE_API_KEY = os.environ.get("BINANCE_API_KEY")
-BINANCE_SECRET_KEY = os.environ.get("BINANCE_SECRET_KEY")

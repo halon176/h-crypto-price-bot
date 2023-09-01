eb949651bbad0138b65152372b033b8f3fb61fad
halon176 <acc.github@halon.cc>
2023-09-01 20:10:25 +0200

    parentesis typo

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ import os
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 ETHSCAN_API_KEY = os.environ.get("ETHSCAN_API_KEY")
-CMC_API_KEY = os.environ.get(("CMC_API_KEY"))
+CMC_API_KEY = os.environ.get("CMC_API_KEY")

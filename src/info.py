a1f04900bb0d397f63d37a8394289c1986525eba
halon176 <acc.github@halon.cc>
2023-12-09 17:02:07 +0100

    changed news feed

--- src/info.py
+++ src/info.py
@@ -34,7 +34,7 @@ async def bot_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "/cmckey - to receive info about CoinMarketCap api key usage\n"
         "/dom - to receive the top 10 most capitalized tokens\n"
         "/gas - to receive real-time gas information on ERC\n"
-        "/news - to receive CoinTelegraph news\n"
+        "/news - to receive CoinDesk news\n"
         "/help - to receive this message\n\n"
         "This bot is written with open-source and free code, and you can find it all at "
         "[GitHub](https://github.com/halon176/h-crypto-price-bot)\n\n"
